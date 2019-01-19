52e41964d4f9b8a38e320538a4e636789f837d92
Kyle MacNeney <eazymac25@yahoo.com>
2019-01-19 16:52:50 -0500

    adding csv data

--- classifiers/decision_tree.py
+++ classifiers/decision_tree.py
@@ -3,6 +3,8 @@ Decision Tree Classifiers for two data sets:
     - U.S. Census
     - Something else
 """
+import os
+
 import numpy as np
 import pandas as pd
 import requests
@@ -12,7 +14,44 @@ from sklearn.tree import DecisionTreeClassifier
 
 
 class CensusDecisionTree(object):
+    raw_data_columns = [
+        'age', 'workclass', 'fnwgt',
+        'education', 'education-num',
+        'marital-status', 'occupation', 'relationship',
+        'race', 'sex', 'capital-gain',
+        'capital-loss', 'hours-per-week',
+        'native-country', 'flag'
+    ]
 
-    def __init__(self):
-        pass
+    raw_data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
+
+    csv_filename = "raw_census_data.csv"
+
+    def __init__(self, raw_df=None):
+        self.raw_df = raw_df
+
+    def download_archive_and_save_as_csv(self):
+        print(os.listdir("./"))
+        if self.csv_filename in os.listdir("./"):
+            raise Exception("File already exists")
+        with open(self.csv_filename, "w") as raw_census_data:
+            raw_census_data.write(','.join(self.raw_data_columns) + '\n')
+            raw_census_data.writelines(
+                requests.get(self.raw_data_url).text
+            )
+
+    def setup(self):
+        """
+        Fetches the data from archives and loads to a data frame
+        :return: void
+        """
+
+        self.raw_df = pd.read_csv("census_data.csv")
+
+
+if __name__ == '__main__':
+
+    dt = CensusDecisionTree()
+    # dt.download_archive_and_save_as_csv()
+
 
